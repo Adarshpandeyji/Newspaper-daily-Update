b4a61b8a51a5441ae652b6f319c13dba803dc8e4
Adarshpandeyji <123234273+Adarshpandeyji@users.noreply.github.com>
2023-08-11 13:32:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,76 @@
 import requests
 from bs4 import BeautifulSoup
 from telegram import Bot, Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, ConversationHandler, MessageHandler, Filters
 
 # Replace 'YOUR_BOT_TOKEN' with your actual Telegram Bot API token
 BOT_TOKEN = '6304691403:AAFjHrYRlbf8Z9ysJJ0kgTj5GTLbknvw_5c'
 CHANNEL_ID = '@newspapertest'  # Replace with your channel username or ID
 DAILY_EPAPER_URL = 'https://www.dailyepaper.in/news-home/'  # Website URL where daily newspapers are hosted
 
-def get_latest_newspaper_link():
+# States for the conversation
+SELECTING_PAPER = 1
+
+def get_available_papers():
     response = requests.get(DAILY_EPAPER_URL)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, 'html.parser')
-    latest_newspaper_link = soup.select_one('.newsdate > a')['href']
-    return latest_newspaper_link
+    papers = soup.select('.newsdate > a')
+    return [paper['href'] for paper in papers]
+
+def start(update: Update, _: CallbackContext):
+    update.message.reply_text("Welcome to the Daily Newspaper Bot! Type /todaynewspaper to receive today's e-paper options.")
+    return SELECTING_PAPER
+
+def todaynewspaper(update: Update, context: CallbackContext):
+    available_papers = get_available_papers()
+
+    if not available_papers:
+        update.message.reply_text("No e-papers available today.")
+        return ConversationHandler.END
+
+    options_text = "\n".join([f"{i + 1}. {paper}" for i, paper in enumerate(available_papers)])
+    update.message.reply_text(f"Today's available e-papers:\n{options_text}\n\nPlease choose a paper by typing its number.")
+
+    context.user_data['available_papers'] = available_papers
+    return SELECTING_PAPER
+
+def select_paper(update: Update, context: CallbackContext):
+    selected_paper_index = int(update.message.text) - 1
+    available_papers = context.user_data.get('available_papers', [])
+
+    if 0 <= selected_paper_index < len(available_papers):
+        selected_paper_link = available_papers[selected_paper_index]
+        newspaper = download_newspaper(selected_paper_link)
+        bot = Bot(token=BOT_TOKEN)
+        bot.send_document(chat_id=CHANNEL_ID, document=newspaper, caption="Here's the selected e-paper!")
+    else:
+        update.message.reply_text("Invalid selection. Please choose a valid paper number.")
+
+    return ConversationHandler.END
 
 def download_newspaper(link):
     response = requests.get(link)
     response.raise_for_status()
     return response.content
 
-def start(update: Update, _: CallbackContext):
-    update.message.reply_text("Welcome to the Daily Newspaper Bot!")
-
-def todaynewspaper(update: Update, _: CallbackContext):
-    try:
-        latest_newspaper_link = get_latest_newspaper_link()
-        newspaper = download_newspaper(latest_newspaper_link)
-        bot = Bot(token=BOT_TOKEN)
-        bot.send_document(chat_id=CHANNEL_ID, document=newspaper, caption="Here's today's newspaper!")
-    except Exception as e:
-        update.message.reply_text("Sorry, there was an error while sending the newspaper. Please try again later.")
-
 def main():
     updater = Updater(BOT_TOKEN)
     dispatcher = updater.dispatcher
 
-    # Add command handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("todaynewspaper", todaynewspaper))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            SELECTING_PAPER: [CommandHandler('todaynewspaper', todaynewspaper)],
+            MessageHandler(Filters.text & ~Filters.command, select_paper),
+        },
+        fallbacks=[],
+    )
+
+    dispatcher.add_handler(conv_handler)
 
     updater.start_polling()
     updater.idle()
 
 if __name__ == "__main__":
     main()
-    
