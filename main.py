1a6d9ae98f365eb23eafa84178cace7183dcd4f4
Adarshpandeyji <123234273+Adarshpandeyji@users.noreply.github.com>
2023-08-11 13:10:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,17 @@ from bs4 import BeautifulSoup
 from telegram import Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-
 # Replace 'YOUR_BOT_TOKEN' with your actual Telegram Bot API token
-BOT_TOKEN = '6304691403:AAFjHrYRlbf8Z9ysJJ0kgTj5GTLbknvw_5c'
-CHANNEL_ID = '@newspapertest'  # Replace with your channel username or ID
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
+CHANNEL_ID = '@your_channel_name'  # Replace with your channel username or ID
 DAILY_EPAPER_URL = 'https://www.dailyepaper.in/news-home/'  # Website URL where daily newspapers are hosted
 
-def get_newspaper_link():
+def get_today_newspaper_link():
     response = requests.get(DAILY_EPAPER_URL)
     response.raise_for_status()
     soup = BeautifulSoup(response.content, 'html.parser')
-    newspaper_link = soup.select_one('.title > a')['href']
-    return newspaper_link
+    today_newspaper_link = soup.select_one('.newsdate > a')['href']
+    return today_newspaper_link
 
 def download_newspaper(link):
     response = requests.get(link)
@@ -27,10 +26,10 @@ def start(update: Update, _: CallbackContext):
 
 def todaynewspaper(update: Update, _: CallbackContext):
     try:
-        newspaper_link = get_newspaper_link()
-        newspaper = download_newspaper(newspaper_link)
+        today_newspaper_link = get_today_newspaper_link()
+        newspaper = download_newspaper(today_newspaper_link)
         bot = Bot(token=BOT_TOKEN)
-        bot.send_document(chat_id=CHANNEL_ID, document=newspaper, caption="Here's today's newspaper!")
+        bot.send_document(chat_id=CHANNEL_ID, document=newspaper, caption="Today's newspaper!")
     except Exception as e:
         update.message.reply_text("Sorry, there was an error while sending the newspaper. Please try again later.")
 
@@ -47,4 +46,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-                   
+    
