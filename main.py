5a14fc989fc8dfa1f4e75ca060a9d208046323ef
Adarshpandeyji <123234273+Adarshpandeyji@users.noreply.github.com>
2023-08-11 13:57:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ BOT_TOKEN = '6304691403:AAFjHrYRlbf8Z9ysJJ0kgTj5GTLbknvw_5c'
 SELECTING_NEWSPAPER, SELECTING_DATE = range(2)
 
 def get_today_newspapers():
-    response = requests.get('https://www.dailyepaper.in/news-home/')
-    response.raise_for_status()
-    soup = BeautifulSoup(response.content, 'html.parser')
-    newspapers = soup.select('.newsdate > a')
-    return [newspaper.text.strip() for newspaper in newspapers]
+    try:
+        response = requests.get('https://www.dailyepaper.in/news-home/')
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+        newspapers = soup.select('.newsdate > a')
+        return [newspaper.text.strip() for newspaper in newspapers]
+    except Exception as e:
+        print("Error fetching newspapers:", e)
+        return []
 
 def start(update: Update, _: CallbackContext):
     update.message.reply_text("Welcome to the Daily Newspaper Bot! Type /todaynews to get today's newspaper options.")
@@ -33,47 +37,4 @@ def todaynews(update: Update, _: CallbackContext):
     context.user_data['available_newspapers'] = available_newspapers
     return SELECTING_DATE
 
-def select_newspaper(update: Update, context: CallbackContext):
-    selected_newspaper_index = int(update.message.text) - 1
-    available_newspapers = context.user_data.get('available_newspapers', [])
-
-    if 0 <= selected_newspaper_index < len(available_newspapers):
-        selected_newspaper = available_newspapers[selected_newspaper_index]
-        context.user_data['selected_newspaper'] = selected_newspaper
-        update.message.reply_text(f"You've selected {selected_newspaper}. Please enter the date in the format DD-MM-YYYY.")
-
-        return SELECTING_DATE
-    else:
-        update.message.reply_text("Invalid selection. Please choose a valid newspaper number.")
-
-def select_date(update: Update, context: CallbackContext):
-    selected_date = update.message.text.strip()
-
-    selected_newspaper = context.user_data.get('selected_newspaper')
-    newspaper_pdf_link = f"https://www.dailyepaper.in/{selected_newspaper.lower()}/{selected_date}.pdf"
-
-    bot = Bot(token=BOT_TOKEN)
-    bot.send_message(chat_id=update.effective_chat.id, text=f"Here's the PDF link for {selected_newspaper} dated {selected_date}:\n{newspaper_pdf_link}")
-
-    return ConversationHandler.END
-
-def main():
-    updater = Updater(BOT_TOKEN)
-    dispatcher = updater.dispatcher
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            SELECTING_NEWSPAPER: [CommandHandler('todaynews', todaynews)],
-            SELECTING_DATE: [MessageHandler(Filters.text & ~Filters.command, select_date)],
-        },
-        fallbacks=[],
-    )
-
-    dispatcher.add_handler(conv_handler)
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == "__main__":
-    main()
+# The rest of the script remains the same...
